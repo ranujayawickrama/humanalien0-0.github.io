a8ad045124ec896bcdf880066f23409a129e1951
humanalien0-0 <152632971+humanalien0-0@users.noreply.github.com>
2025-03-18 14:16:56 -0600

    added a net to catch the bug and switched the circle to a actual bug

--- arrays-assignment/sketch.js
+++ arrays-assignment/sketch.js
@@ -9,89 +9,52 @@
 // - describe what you did to take this project "above and beyond"
 
 
-
-// let bug;
-// let bugWidth;
-// let bugHeight;
-// let timeX;
-// let timeY;
-// let bugX;
-// let bugY;
-// let bugSpeed = 0.01;
-
-// let bugBg;
-
-// //loading images
-// function preload() {
-//   bug = loadImage("firefly.png");
-//   bugBg = loadImage("bugBg.jpg");
-// }
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-
-//   bugX = width/2;
-//   bugY = height/2;
-
-//   timeX = random(0, 100);
-//   timeY = random(500, 1000);
-
-// }
-
-// function draw() {
-//   background(220);
-//   displayImages();
-//   bugMovement();
-
-// }
-
-// function displayImages() {
-//   image(bugBg, 0, 0, windowWidth, windowHeight);
-// }
-
-// function bugMovement(){
-//   bugX = noise(timeX)* width;
-//   bugY = noise(timeY)* height;
-//   image(bug, bugX, bugY, bugWidth, bugHeight);
-
-//   timeX+= bugSpeed;
-//   timeY+= bugSpeed;
-// }
-
-//lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-
 let bubbles = [];
+
 let bugX;
 let bugY;
 let bug;
 let bugWidth;
 let bugHeight;
+
+let net;
+let netWidth;
+let netHeight;
+
 let bugBg;
+let score = 0;
 
 //loading images
 function preload() {
   bug = loadImage("firefly.png");
   bugBg = loadImage("bugBg.jpg");
+  net = loadImage("net.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   bugWidth = bug.width * height * 0.0002 ;
-  bugHeight = bug.width * height * 0.0002 ;
+  bugHeight = bug.width * height * 0.0002;
+  netWidth = net.width * height * 0.0004; 
+  netHeight = net.height * height * 0.0005; 
+  console.log(bugWidth, bugHeight);
+  noCursor();
 
   for (let i = 0; i < 3 ; i ++){
     spawnBubble();
   }
   // spawn a new bubble every half a second
-  window.setInterval(spawnBubble, 2000);
+  window.setInterval(spawnBubble, 1500);
+ 
 
 }
 function mousePressed(){
   for (let bub of bubbles){
     // if this bubble is clicked on 
-    if (dist(mouseX, mouseY, bub.x, bub.y) < 27){
+    if (dist(mouseX, mouseY, bub.x , bub.y ) < 28){
       let index = bubbles.indexOf(bub);
       bubbles.splice(index, 1);
+      score++;
     };
   }
 }
@@ -99,23 +62,14 @@ function mousePressed(){
 function draw() {
   background(220);
   displayImages();
-  for (let bub of bubbles){
-
-    bub.x = noise(bub.timeX) * width;
-    bub.y = noise(bub.timeY) * height;
-    
-    fill("green");
-    noStroke();
-    circle(bub.x, bub.y, 50);
-
-    // move bubble
-    bub.timeX += bub.bugSpeed;
-    bub.timeY += bub.bugSpeed;
-  }
+  bugMovement();
+  displayScore();
 }
 
 function displayImages() {
+  imageMode(CORNER);
   image(bugBg, 0, 0, windowWidth, windowHeight);
+  image(net, mouseX-30, mouseY-50, netWidth, netHeight );
 }
 
 function spawnBubble(){
@@ -123,9 +77,41 @@ function spawnBubble(){
     timeX: random(0, 1000),  // Still needed for movement
     timeY: random(0, 1000),
     
-    bugSpeed: random(0.005, 0.008),
+    bugSpeed: random(0.004, 0.008),
     x: random(width),  // Start at random x
     y: random(height), // Start at random y
   };
   bubbles.push(someb);
+  console.log(bubbles.length);
 }
+function bugMovement(){
+  for (let bub of bubbles){
+
+    bub.x = noise(bub.timeX) * width;
+    bub.y = noise(bub.timeY) * height;
+    
+    imageMode(CENTER);
+    image(bug, bub.x, bub.y, bugWidth, bugHeight);
+    
+
+    // move bubble
+    bub.timeX += bub.bugSpeed;
+    bub.timeY += bub.bugSpeed;
+  }
+}
+function displayScore() {
+  //setting up varibles so that the background for the score  will be drawn in any winodw size
+  let rectWidth = width * 0.2;
+  let rectHeight = height * 0.05;
+  let rectX = width * 0.02;
+  let rectY = height * 0.02;
+
+  fill("lightgreen");
+  rect(rectX, rectY, rectWidth, rectHeight);
+
+  fill(0);
+  textSize(width / 55);
+  textAlign(CENTER, CENTER);
+  text("Score: " + score, rectX + rectWidth / 2, rectY + rectHeight / 2); // Display score text
+}
+
