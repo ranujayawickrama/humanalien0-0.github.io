5643d618e31af0f4f0c210e2696f0c7a23e01729
humanalien0-0 <7009611@spsd.sk.ca>
2025-03-17 23:30:29 -0600

    completed the movement of the bugs - perlin noise

--- arrays-assignment/sketch.js
+++ arrays-assignment/sketch.js
@@ -10,17 +10,63 @@
 
 
 
+// let bug;
+// let bugWidth;
+// let bugHeight;
+// let timeX;
+// let timeY;
+// let bugX;
+// let bugY;
+// let bugSpeed = 0.01;
+
+// let bugBg;
+
+// //loading images
+// function preload() {
+//   bug = loadImage("firefly.png");
+//   bugBg = loadImage("bugBg.jpg");
+// }
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+
+//   bugX = width/2;
+//   bugY = height/2;
+
+//   timeX = random(0, 100);
+//   timeY = random(500, 1000);
+
+// }
+
+// function draw() {
+//   background(220);
+//   displayImages();
+//   bugMovement();
+
+// }
+
+// function displayImages() {
+//   image(bugBg, 0, 0, windowWidth, windowHeight);
+// }
+
+// function bugMovement(){
+//   bugX = noise(timeX)* width;
+//   bugY = noise(timeY)* height;
+//   image(bug, bugX, bugY, bugWidth, bugHeight);
+
+//   timeX+= bugSpeed;
+//   timeY+= bugSpeed;
+// }
+
+//lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+let bubbles = [];
+let bugX;
+let bugY;
 let bug;
 let bugWidth;
 let bugHeight;
-let timeX = 0;
-let timeY = 1000;
-let bugX;
-let bugY;
-let bugSpeed = 0.015;
-
 let bugBg;
-let bubbles = [];
 
 //loading images
 function preload() {
@@ -32,33 +78,39 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   bugWidth = bug.width * height * 0.0002 ;
   bugHeight = bug.width * height * 0.0002 ;
-  bugX = width/2;
-  bugY = height/2;
 
-  for (let i = 0; i < 10 ; i ++){
+  for (let i = 0; i < 3 ; i ++){
     spawnBubble();
   }
   // spawn a new bubble every half a second
-  window.setInterval(spawnBubble, 500);
+  window.setInterval(spawnBubble, 2000);
+
+}
+function mousePressed(){
+  for (let bub of bubbles){
+    // if this bubble is clicked on 
+    if (dist(mouseX, mouseY, bub.x, bub.y) < 27){
+      let index = bubbles.indexOf(bub);
+      bubbles.splice(index, 1);
+    };
+  }
 }
 
 function draw() {
   background(220);
   displayImages();
-  bugMovement();
-
   for (let bub of bubbles){
-    //randomize movement - too much coffee 
-    bub.dx = random(-5, 5);
-    bub.dy = random(-5, 5);
+
+    bub.x = noise(bub.timeX) * width;
+    bub.y = noise(bub.timeY) * height;
+    
+    fill("green");
+    noStroke();
+    circle(bub.x, bub.y, 50);
 
     // move bubble
-    bub.x += bub.dx;
-    bub.y += bub.dy;
-
-    fill(bub.r, bub.g, bub.b);
-    noStroke();
-    circle(bub.x, bub.y, bub.radius*2);
+    bub.timeX += bub.bugSpeed;
+    bub.timeY += bub.bugSpeed;
   }
 }
 
@@ -66,41 +118,14 @@ function displayImages() {
   image(bugBg, 0, 0, windowWidth, windowHeight);
 }
 
-function bugMovement(){
-  bugX = noise(timeX)* width;
-  bugY = noise(timeY)* height;
-  image(bug, bugX, bugY, bugWidth, bugHeight);
-
-  timeX+= bugSpeed;
-  timeY+= bugSpeed;
-}
-
-
-//DNJGJVABNGJBGIJGAIOUHAUEBHUVJGHJBHNBAJOBJFI;llllllijjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-
-
-function mousePressed(){
-  for (let bub of bubbles){
-    // if this bubble is clicked on 
-    if (dist(mouseX, mouseY, bub.x, bub.y) < bub.radius){
-      let index = bubbles.indexOf(bub);
-      bubbles.splice(index, 1);
-    };
-  }
-}
-
 function spawnBubble(){
-  let someb= {
-    x: random(windowWidth),
-    y: random(windowHeight),
-    radius: random(40, 80),
-
-    r: random(255),
-    g: random(255),
-    b: random(255),
-
-    dx: random(-5, 5),
-    dy: random(-5, 5),
+  let someb = {
+    timeX: random(0, 1000),  // Still needed for movement
+    timeY: random(0, 1000),
+    
+    bugSpeed: random(0.005, 0.008),
+    x: random(width),  // Start at random x
+    y: random(height), // Start at random y
   };
   bubbles.push(someb);
 }
