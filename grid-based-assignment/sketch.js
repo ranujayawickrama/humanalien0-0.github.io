a54368214ba129bc3c49c7dd117388aca8ae7f9e
humanalien0-0 <7009611@spsd.sk.ca>
2025-03-31 23:29:19 -0600

    Created a chess board and centered it

--- grid-based-assignment/sketch.js
+++ grid-based-assignment/sketch.js
@@ -1,34 +1,66 @@
-// chess
-// Ranu Jayawickrama
-// March 26th
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// chess game
+// Ranu jayawickrama
+// March 28th
 
-const SQUARE_SIZE = 80;
-let blockColour = "is white";
+// extras for experts - multiplayer, popup screen
+
+let cellSize;
+const CHESSBOARD_DIMENSIONS = 8;
+let grid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  //make the largest square that fits
+  if (height > width) {
+    cellSize = width / CHESSBOARD_DIMENSIONS;
+  }
+  else {
+    cellSize = height / CHESSBOARD_DIMENSIONS;
+  }
+
+  grid = generateChessBoard(CHESSBOARD_DIMENSIONS, CHESSBOARD_DIMENSIONS);
 }
 
 function draw() {
   background(220);
-  drawBoard();
+  displayChessBoard();
 }
 
-function drawBoard(){
-  for (let y = 0; y<=8; y++){
-    for(let x = 0; x<=8; x++){
-      if(blockColour === "is white"){
-        fill(255);
-        blockColour = "is black";
+function displayChessBoard() {
+
+  for (let y = 0; y < CHESSBOARD_DIMENSIONS; y++) {
+    for (let x = 0; x < CHESSBOARD_DIMENSIONS; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
       }
-      else if(blockColour === "is black"){
-        fill(0);
-        blockColour = "is white";
+      else if (grid[y][x] === 0) {
+        fill("white");
       }
-      rect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE);
+       rect
+      rect(x * cellSize, y * cellSize, cellSize);
     }
   }
 }
+
+function generateChessBoard(cols, rows) {
+  let aChessBoard = [];
+  let isWhite = true;
+  for (let y = 0; y < rows; y++) {
+    aChessBoard.push([]);
+    isWhite = !isWhite;//change the pattern when starting a new row
+    for (let x = 0; x < cols; x++) {
+
+      if (isWhite) {
+        aChessBoard[y].push(0);
+        isWhite = false;
+      }
+      else {
+        aChessBoard[y].push(1);
+        isWhite = true;
+      }
+    }
+  }
+  return aChessBoard;
+}
+
